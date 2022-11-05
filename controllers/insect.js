db7f3cc3e1f81ed84097204de5b66189fe425dfd
Konda Shivaradhan <38292566+KondaShivaradhan@users.noreply.github.com>
2022-11-05 15:10:39 -0500

    Assignemtn 11 done

--- controllers/insect.js
+++ controllers/insect.js
@@ -1,15 +1,36 @@
 var Insect = require('../models/insect');
 // List of all Insects
-exports.insect_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Insect list');
+exports.insect_list = async function(req, res) {
+    try {
+        theInsects = await Insect.find();
+        res.send(theInsects);
+    } catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
 // for a specific Insect.
 exports.insect_detail = function(req, res) {
     res.send('NOT IMPLEMENTED: Insect detail: ' + req.params.id);
 };
 // Handle Insect create on POST.
-exports.insect_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Insect create POST');
+exports.insect_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Insect();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"insect_Name":"goat", "cost":12, "size":"large"}
+    document.insect_Name = req.body.insect_Name;
+    document.insect_variants = req.body.insect_variants;
+    document.insect_loc = req.body.insect_loc;
+    try {
+        let result = await document.save();
+        res.send(result);
+    } catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
 // Handle Insect delete form on DELETE.
 exports.insect_delete = function(req, res) {
@@ -18,4 +39,13 @@ exports.insect_delete = function(req, res) {
 // Handle Insect update form on PUT.
 exports.insect_update_put = function(req, res) {
     res.send('NOT IMPLEMENTED: Insect update PUT' + req.params.id);
+};
+exports.insect_view_all_Page = async function(req, res) {
+    try {
+        theInsects = await Insect.find();
+        res.render('insects', { title: 'Insect Search Results', results: theInsects });
+    } catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
--- routes/insects.js
+++ routes/insects.js
@@ -1,9 +1,13 @@
 var express = require('express');
+const insect_controlers = require('../controllers/insect');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('insects', { title: 'Insects Search Results' });
-});
+router.get('/', insect_controlers.insect_view_all_Page);
+
+// router.get('/', function(req, res, next) {
+//     res.render('insects', { title: 'Insects Search Results' });
+
+// });
 
 module.exports = router;
